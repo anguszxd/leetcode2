0bf5eb55104448393af3c5291865c981dd7ead41
anguszxd <zxd0520@163.com>
2015-08-18 21:57:52 +0800

    finial version

--- SearchInsertPosition.py
+++ SearchInsertPosition.py
@@ -11,10 +11,35 @@
 #[1,3,5,6], 7 → 4
 #[1,3,5,6], 0 → 0
 
+#原理还是二分法，由于target可能不在数组中，要返回其可能的位置
+#使用二分法会不断接近target应该在的位置，当target不在数组中时，
+#left、right指针的变动会使得搜索段直接跳过target
+#因此要找到target所在的位置需要注意指针变动的情况
 def searchInsert(nums, target):
-	
+	left, right = 0, len(nums)-1
 
-nums = [1,3,5,6]
-target = 2
+	if target < nums[0]: return 0
+	if target > nums[-1]: return len(nums)
+
+	while left <= right:
+		mid = (left + right) >> 1
+
+		if target > nums[mid]:
+			#mid指针变动使得搜索段跳过target，
+			#这就表示target在mid后mid+1之前
+			if target < nums[mid+1]:
+				return mid+1
+			left = mid + 1
+		elif target < nums[mid]:
+			if target > nums[mid-1]:
+				return mid
+			right = mid - 1
+		else:
+			return mid
+		
+
+
+nums = [1]
+target = 0
 
 print searchInsert(nums,target)
