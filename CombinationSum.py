6e3b2e075ff507a1b5543ec9639ec5a9c0073f88
anguszxd <zxd0520@163.com>
2015-08-19 14:45:43 +0800

    add note

--- CombinationSum.py
+++ CombinationSum.py
@@ -14,7 +14,17 @@
 #[2, 2, 3] 
 
 
-def combinationAll(s,t,ans,branch):
+#Solution1:用找组合的方式逐步寻找符合要求的结果
+#找到的组合应该是所有小于等于target的候选数字的组合
+#如candidate set [2,3,6,7]，target=7
+#第一次找到的组合是:[[2],[3],[2,3],[7]]
+#只有在这些组合的基础上才可能出现满足要求的结果
+#下一步是在某个组合的基础上继续上面的过程，
+#如取[2]，继续在原来的优选数字中找组合加到[2]上，使其满足小于等于target
+#第二次搜索的结果是：[[2,2],[2,3]]
+#以上是迭代的过程，退出条件：1组合求和正好等于target，加入到ans中（如果原先ans中没有这个答案的话）
+#2继续找下去满足加和小于等于target的组合的数量为0
+def combinationSum(s,t,ans,branch):
 	if sum(branch) == t:
 		branch = sort(branch)
 		if branch not in ans:
@@ -47,8 +57,6 @@ def combination(res, path, n, sleft,target):
 	combination(res,path,n,sleft[1:],target)
 
 
-
-
 def sort(subans):
 	sort_res = []
 #	subans.index(min(subans))
@@ -68,5 +76,5 @@ t = 7
 ans = []
 branch = []
 #print combinationSum(can,t)
-combinationAll(can,t,ans,branch)
+combinationSum(can,t,ans,branch)
 print ans
