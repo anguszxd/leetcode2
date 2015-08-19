b23a25b61f4d7645aa593dadf0e5865e2b70a338
anguszxd <zxd0520@163.com>
2015-08-19 09:48:01 +0800

    add a new solution using combination, easy to understand but a bit slow

--- ThreeSum.py
+++ ThreeSum.py
@@ -113,7 +113,35 @@ def twoSum(num, target):
         process[num[index]]=index
 
 
+#先找到数组的所有三元素的组合，再对这些组合进行检查，看是否满足和为0
+#很容易理解，但效率较低，在OJ中超时
+def CombinatinThreeSum(nums):
+	comb = []
+	path = []
+	res = []
+	nums = sort(nums)
+	combination(comb,path,3,nums)
+	for i in range(len(comb)):
+		if sum(comb[i])==0 and comb[i] not in res:
+			res.append(comb[i])
+
+	return res
+
+
+def combination(res, path, n, sleft):
+	if len(sleft) < n:
+		return
+	if n == 0 or len(sleft) == 0:
+		res.append(path)
+		return
+
+	combination(res,path+[sleft[0]],n-1,sleft[1:])
+	combination(res,path,n,sleft[1:])
+
 
 S = [8,5,12,3,-2,-13,-8,-9,-8,10,-10,-10,-14,-5,-1,-8,-7,-12,4,4,10,-8,0,-3,4,11,-9,-2,-7,-2,3,-14,-12,1,-4,-6,3,3,0,2,-9,-2,7,-8,0,14,-1,8,-13,10,-11,4,-13,-4,-14,-1,-8,-7,12,-8,6,0,-15,2,8,-4,11,-4,-15,-12,5,-9,1,-2,-10,-14,-11,4,1,13,-1,-3,3,-7,9,-4,7,8,4,4,8,-12,12,8,5,5,12,-7,9,4,-12,-1,2,5,4,7,-2,8,-12,-15,-1,2,-11]
-print threeSum3(S)
+#S = [-2,-1,-1,0,0,0,2]
+n = [-13,11,11,0,-5,-14,12,-11,-11,-14,-3,0,-3,12,-1,-9,-5,-13,9,-7,-2,9,-1,4,-6,-13,-7,10,10,9,7,13,5,4,-2,7,5,-13,11,10,-12,-14,-5,-8,13,2,-2,-14,4,-8,-6,-13,9,8,6,10,2,6,5,-10,0,-11,-12,12,8,-7,-4,-9,-13,-7,8,12,-14,10,-10,14,-3,3,-15,-14,3,-14,10,-11,1,1,14,-11,14,4,-6,-1,0,-11,-12,-14,-11,0,14,-9,0,7,-12,1,-6]
+print CombinatinThreeSum(n)
+#print threeSum3(S)
 #print sort(S)
