44949072dab0e51a2150981ac8f7f1f11c782805
anguszxd <zxd0520@163.com>
2015-08-22 16:34:19 +0800

    create the file and add an easy solution

---
+++ MultiplyString.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+#Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+#Note: The numbers can be arbitrarily large and are non-negative.
+
+def multiply(num1,num2):
+	return str(int(num1)*int(num2))
+
+def multiply2(num1,num2):
+
+
+
+n1 = 14
+n2 = 23
+print multiply2(n1,n2)
